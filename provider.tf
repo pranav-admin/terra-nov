f3be023185d7e85bea6cff7bdfa86c580b18ffc0
Pranav <154361136+pranav-admin@users.noreply.github.com>
2025-11-21 13:31:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,9 +9,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "2df30ff1-915d-4d35-974a-3d3155aaa413"
-  client_id       = "1624bd86-8bac-41c7-9597-83a619e7faa2"
-  client_secret   = "HVW8Q~PQTALBWovaJLo2QdPeEcGFPK.goVsOTcOr"
-  tenant_id       = "cabf5d06-7e3a-4d14-b7f4-2b5819ed881e"
-
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
