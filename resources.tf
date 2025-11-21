467c2811acda9dfcd0a1af3c7d4b9777d4792b98
pranav <pranav@blah.com>
2025-11-21 11:28:50 +0530

    Added code for NIC

--- resources.tf
+++ resources.tf
@@ -28,3 +28,16 @@ resource "azurerm_public_ip" "linux-pip" {
     create_before_destroy = true
   }
 }
+
+resource "azurerm_network_interface" "linux-nic" {
+  name                = "linux-nic"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.linux-subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.linux-pip.id
+  }
+}
